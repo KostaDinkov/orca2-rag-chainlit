efecda1f432561f77e20c438cc9d74b401da8282
Kostadin Dinkov <k.v.dinikov@gmail.com>
2023-12-04 21:58:45 +0200

    update project

--- load_data_vdb.py
+++ load_data_vdb.py
@@ -1,10 +1,7 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
-from langchain.document_loaders import UnstructuredHTMLLoader, BSHTMLLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import GPT4AllEmbeddings
-from langchain.embeddings import OllamaEmbeddings  
 
 import os
 
@@ -12,12 +9,18 @@ DATA_PATH="data/"
 DB_PATH = "vectorstores/db/"
 
 def create_vector_db():
+    # зареждаме наличните в папката data pdf документи
     loader = PyPDFDirectoryLoader(DATA_PATH)
     documents = loader.load()
     print(f"Processed {len(documents)} pdf files")
+
+    # 'нарязваме' документите на парчета
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     texts=text_splitter.split_documents(documents)
-    vectorstore = Chroma.from_documents(documents=texts, embedding=GPT4AllEmbeddings(),persist_directory=DB_PATH)      
+
+    # създаваме 'вграждания' (embeddings) и ги запазваме във векторната база данни
+    vectorstore = Chroma.from_documents(documents=texts, embedding=GPT4AllEmbeddings(), 
+                                        persist_directory=DB_PATH)      
     vectorstore.persist()
 
 if __name__=="__main__":
--- rag.py
+++ rag.py
@@ -1,70 +1,81 @@
 #import required dependencies
-from langchain import hub
+from langchain.prompts import PromptTemplate
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import chainlit as cl
-from langchain.chains import RetrievalQA,RetrievalQAWithSourcesChain
+from langchain.chains import RetrievalQA
 
 # шаблон, който ще използваме при генерирането на потребителския промпт
-QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt")
+template = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:"""
 
-# функция за зареждане на Orca2 ГЕМ чрез Ollama
-def load_llm():
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+DB_PATH = "vectorstores/db/"
+
+
+def get_qa_chain(): 
+ 
+ # дефинираме езиковия модел - в случая използваме Orca2 на Microsoft
  llm = Ollama(
  model="orca2",
  verbose=True,
  callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
  )
- return llm
-
-# функция за създаване на верига 'chain' която ще обработва
-def retrieval_qa_chain(llm, vectorstore):
- qa_chain = RetrievalQA.from_chain_type(
+ 
+ # дефинираме базата данни за векторите  
+ vectorstore = Chroma(persist_directory=DB_PATH, embedding_function=GPT4AllEmbeddings())
+ 
+ # конфигурираме веригата (chain) през която протича заявката на потребителя.
+ qa = RetrievalQA.from_chain_type(
     llm,
     retriever=vectorstore.as_retriever(),
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-    return_source_documents=True,
-)
- return qa_chain
-
-
-def qa_bot(): 
- llm=load_llm() 
- DB_PATH = "vectorstores/db/"
- vectorstore = Chroma(persist_directory=DB_PATH, embedding_function=GPT4AllEmbeddings())
- qa = retrieval_qa_chain(llm,vectorstore)
+    return_source_documents=True,)
  return qa 
 
+# когато chainlit клиента стартира
 @cl.on_chat_start
 async def start():
- chain=qa_bot()
+ chain=get_qa_chain()
  msg=cl.Message(content="Starting the chatbot...")
  await msg.send()
- msg.content= "Welcome to the Information Systems chat bot. What is your question?"
+ msg.content = "Welcome to the Information Systems chat bot. What is your question?"
  await msg.update()
  cl.user_session.set("chain",chain)
 
+# когато изпратим съобщение в chainlit клиента 
 @cl.on_message
 async def main(message):
- chain=cl.user_session.get("chain")
- cb = cl.AsyncLangchainCallbackHandler(
- stream_final_answer=True,
- answer_prefix_tokens=["FINAL", "ANSWER"]
- )
- cb.answer_reached=True
  
- res=await chain.acall(message.content, callbacks=[cb])
- print(f"response: {res}")
- answer=res["result"]
- answer=answer.replace(".",".\n")
- sources=res["source_documents"]
+ chain=cl.user_session.get("chain")
+ 
+ cb = cl.AsyncLangchainCallbackHandler(
+    stream_final_answer=True,
+    answer_prefix_tokens=["FINAL", "ANSWER"]
+ )
+ 
+ cb.answer_reached = True
+ 
+ response = await chain.acall(message.content, callbacks=[cb])
+ # отговорът от Orca2
+ answer = response["result"]
+ #answer = answer.replace(".",".\n")
+ 
+ # ако chroma e намерила източници, те ще бъдат включени в този списък
+ sources = response["source_documents"]
 
  if sources:
   answer+=f"\nSources: "+str(str(sources))
  else:
   answer+=f"\nNo Sources found"
 
- await cl.Message(content=answer).send() 
+ await cl.Message(content = answer).send() 
